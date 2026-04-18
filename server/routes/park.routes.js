1c43e7200b7e4843528e654d98d012f6b3d0ed74
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-04-18 20:44:24 +0530

    park routes done

---
+++ server/routes/park.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  createPark,
+  getParks,
+  getPark,
+  updatePark,
+  deletePark
+} from "../controllers/parkController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
+
+const router = Router();
+
+// Public
+router.get("/", getParks);
+router.get("/:id", getPark);
+
+// Protected
+router.post("/", verifyToken, createPark);
+router.put("/:id", verifyToken, updatePark);
+router.delete("/:id", verifyToken, deletePark);
+
+export default router;
